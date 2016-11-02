bdddc34fb8579188074bf68901db406766954b87
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2016-11-02 18:06:17 -0200

    adicionado tipo string

--- src/main/java/com/github/kyriosdata/seed/Seed.java
+++ src/main/java/com/github/kyriosdata/seed/Seed.java
@@ -6,6 +6,7 @@
 
 package com.github.kyriosdata.seed;
 
+import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 
 /**
@@ -208,5 +209,42 @@ public class Seed {
     public char unpackChar(byte[] buffer, int offset) {
         ByteBuffer wrapped = ByteBuffer.wrap(buffer);
         return wrapped.getChar(offset);
+    }
+
+    /**
+     * Empacota um {@code String} em vetor de bytes.
+     *
+     * @param valor Valor a ser empacotado.
+     *
+     * @return Vetor de bytes contendo o valor empacotado.
+     */
+    public byte[] pack(String valor) throws UnsupportedEncodingException {
+        byte[] bytes = valor.getBytes("UTF-8");
+        int tamanho = bytes.length;
+
+        // Guarda tamanho (int) + os bytes da String propriamente dita
+        ByteBuffer buffer = ByteBuffer.allocate(4 + tamanho);
+        buffer.putInt(tamanho);
+        buffer.put(bytes);
+        return buffer.array();
+    }
+
+    /**
+     * Recupera {@code char} depositado no buffer na posição indicada.
+     *
+     * @param buffer Vetor do qual o {@code char} será recuperado.
+     * @param offset Posição inicial do valor a ser recuperado.
+     * @return Valor recuperado do buffer na posição indicada.
+     */
+    public String unpackString(byte[] buffer, int offset) throws UnsupportedEncodingException {
+        ByteBuffer wrapped = ByteBuffer.wrap(buffer);
+
+        int tamanho = wrapped.getInt(offset);
+        byte[] strBytes = new byte[tamanho];
+
+        wrapped.position(4);
+        wrapped.get(strBytes, 0, tamanho);
+
+        return new String(strBytes, "UTF-8");
     }
 }
--- src/test/java/com/github/kyriosdata/seed/SeedTest.java
+++ src/test/java/com/github/kyriosdata/seed/SeedTest.java
@@ -3,6 +3,8 @@ package com.github.kyriosdata.seed;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.UnsupportedEncodingException;
+
 import static org.junit.Assert.assertEquals;
 
 public class SeedTest {
@@ -18,6 +20,16 @@ public class SeedTest {
         assertEquals(5.1, s.unpackDouble(s.pack((double)5.1), 0), 0.0001d);
         assertEquals('a', s.unpackChar(s.pack('a'), 0));
         assertEquals(false, s.unpackBoolean(s.pack(false), 0));
+    }
+
+    @Test
+    public void tipoString() throws UnsupportedEncodingException {
+        Seed s = new Seed();
+
+        assertEquals("casa", s.unpackString(s.pack("casa"), 0));
+
+        String msg = "um longa mensagem, quer dizer, nem tanto!";
+        assertEquals(msg, s.unpackString(s.pack(msg), 0));
     }
 }
 
